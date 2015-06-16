d08184f2f292dc728a40afff62c890c4c43845fe
Daniel Gonzalez <dyg2104@gmail.com>
2015-06-15 20:27:47 -0400

    adds completed background form component

--- assets/javascripts/components/BackgroundSubForm.jsx
+++ assets/javascripts/components/BackgroundSubForm.jsx
@@ -15,7 +15,13 @@ var BackgroundSubForm = React.createClass({
 				<div>background</div>
 			    <TextField getFormObj={this.getFormObj} keyName="title" title="Title" />
 				<TextFieldArray getFormObj={this.getFormObj} keyName="authors" title="Authors" />
-				<TextField getFormObj={this.getFormObj} keyName="description" title="Description" />	
+				<TextField getFormObj={this.getFormObj} keyName="description" title="Description" />
+				<TextField getFormObj={this.getFormObj} keyName="diseaseName" title="Disease Name" />
+				<TextFieldArray getFormObj={this.getFormObj} keyName="diseaseComplications" title="Complications" />
+				<TextFieldArray getFormObj={this.getFormObj} keyName="diseaseCurrentTreatments" title="Current Treatments" />
+			 	<TextFieldArray getFormObj={this.getFormObj} keyName="trials" title="Trials" />
+				<TextFieldArray getFormObj={this.getFormObj} keyName="involvedParties" title="Involved Parties" />
+				<TextFieldArray getFormObj={this.getFormObj} keyName="trials" title="Trials" />
 				<a href="#" onClick={this.props.nextStep}>Next</a>
 			</div>
 		)
