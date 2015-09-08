b6ecbbaa3690b2fe7c2440fe4a2a74c68643bbcd
Daniel Gonzalez <dyg2104@gmail.com>
2015-09-07 22:45:07 -0400

    adds eligibility criteria js file for show page

--- assets/javascripts/show/components/methodology/EligibilityCriterion.jsx
+++ assets/javascripts/show/components/methodology/EligibilityCriterion.jsx
@@ -11,13 +11,43 @@ class EligibilityCriterion extends React.Component {
 		let controlGroups = this.props.controlGroups;
 		let totalData = this.props.totalData;
 		
-		let allCriteria = findNameSpace('methodology.eligibilityCriteria', totalData);
-		let criteria = allCriteria[index];
+		let tdCriteria = findNameSpace('methodology.eligibilityCriteria', totalData);
+		let tdCriterion = tdCriteria[index];
+		
+		let cgLowMarkup = [];
+		let cgHighMarkup = [];
+		
+		for (let controlGroup of controlGroups) {
+			let cgCriteria = findNameSpace('methodology.eligibilityCriteria', controlGroup);
+			let cgCriterion = cgCriteria[index];
+			cgLowMarkup.push(<div>{cgCriterion.low}</div>);
+			cgHighMarkup.push(<div>{cgCriterion.high}</div>);
+		}
 		
 		return (
 			<div>
-				<div>{criteria.name}</div>
-				<div>{criteria.units}</div>
+				<div>
+					<div>Name</div>
+					<div>{tdCriterion.name}</div>
+				</div>
+				<div>
+					<div>Units</div>
+					<div>{tdCriterion.units}</div>
+				</div>
+				<div>
+					<div>Low</div>
+					<div>
+						{cgLowMarkup}
+						<div>{tdCriterion.low}</div>
+					</div>
+				</div>
+				<div>
+					<div>High</div>
+					<div>
+						{cgHighMarkup}
+						<div>{tdCriterion.high}</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
