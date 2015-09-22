83c7b3e80a083d99f749bfca948fe645ae97106b
Daniel Gonzalez <dyg2104@gmail.com>
2015-09-21 21:58:44 -0400

    adds factory for revised new form

--- assets/javascripts/newBao/components/methodology/NewMethodology.jsx
+++ assets/javascripts/newBao/components/methodology/NewMethodology.jsx
@@ -1,13 +1,17 @@
-const React                = require('react');
+const React                 = require('react');
 
-const TextField            = require('../shared/TextField.jsx');
-const TextFieldArray       = require('../shared/TextFieldArray.jsx');
-const factoryCollection  = require('../shared/factoryCollection.jsx');
+const TextField             = require('../shared/TextField.jsx');
+const TextFieldArray        = require('../shared/TextFieldArray.jsx');
+const factoryCollection     = require('../shared/factoryCollection.jsx');
+const factoryCGTD           = require('../shared/factoryCGTD.jsx');
 
-const ControlGroup         = require('./ControlGroup.jsx');
-const ControlGroups        = factoryCollection(ControlGroup);
+const ControlGroup          = require('./ControlGroup.jsx');
+const EligibilityCriterion  = require('./EligibilityCriterion.jsx');
 
-const uiActions            = require('../../actions/uiActions.es.js');
+const ControlGroups         = factoryCollection(ControlGroup);
+const EligibilityCriteria   = factoryCGTD(EligibilityCriterion);
+
+const uiActions             = require('../../actions/uiActions.es.js');
 
 class NewMethodology extends React.Component {
 	constructor(props) {
@@ -17,9 +21,19 @@ class NewMethodology extends React.Component {
 	render() {
 		let study = this.props.study;
 		let methodology;
+		let controlGroups;
+		let totalData;
 		
 		if (study && study.methodology) {
 			methodology = study.methodology.value ? study.methodology.value : {};
+		}
+		
+		if (study && study.controlGroups) {
+			controlGroups = study.controlGroups.value ? study.controlGroups.value : {};
+		}
+		
+		if (study && study.totalData) {
+			totalData = study.totalData.value ? study.totalData.value : {};
 		}
 		
 		return (
@@ -49,6 +63,10 @@ class NewMethodology extends React.Component {
 					getKey="secondaryEndpoints"
 					setKey="methodology:secondaryEndpoints"
 					title="Secondary Endpoints" />
+				<EligibilityCriteria
+					controlGroups={controlGroups}
+					totalData={totalData}
+					title="Eligibility Criteria" />
 				
 					
 				<a href="#" onClick={uiActions.previousStep}>Previous</a>
